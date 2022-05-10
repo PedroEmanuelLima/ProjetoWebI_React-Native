6efb82815c0503db68f4a9018c5da09c1c79b985
Pedro Almeida <pa69731@gmail.com>
2022-05-10 00:03:51 -0300

    build: update expo and npm

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ const Stack = createNativeStackNavigator()
 import { Home } from './src/pages/Home/Home';
 import { Detalhes } from './src/pages/Detalhes/Detalhes';
 import { Cart } from './src/pages/Cart/Cart'
+import { AdressAndPayment } from "./src/pages/AdressAndPayment/AdressAndPayment";
 
 export default function App() {
 
@@ -15,22 +16,32 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home" screenOptions={{
         animation:"slide_from_right"
       }}>
+
         <Stack.Screen name='Home' component={Home} options={{
           headerShown: false,
         }}/>
+
         <Stack.Screen name='Detalhes' component={Detalhes} options={{
           title: 'Produto',
           headerStyle: {backgroundColor: 'rgb(3,147,213)',},
           headerTintColor: '#fff',
           headerTitleAlign: 'center' 
         }}/>
+        
         <Stack.Screen name='Cart' component={Cart} options={{
           title: 'Carrinho',
           headerStyle: {backgroundColor: 'rgb(3,147,213)'},
           headerTintColor: '#fff',
           headerTitleAlign: 'center',
         }}/>
-      </Stack.Navigator>
+
+        <Stack.Screen name='AdressAndPayment' component={AdressAndPayment} options={{
+          title: 'Inormações da Compra',
+          headerStyle: {backgroundColor: 'rgb(3,147,213)'},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+        }}/>
+    </Stack.Navigator>
     </NavigationContainer>
 
   );
