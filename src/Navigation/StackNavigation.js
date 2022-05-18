60ec4f191faf6ba6716fccdca9bf8a17cb94d872
Pedro Almeida <pa69731@gmail.com>
2022-05-18 14:02:49 -0300

    Refactor: Update libs and alter in App.js

---
+++ src/Navigation/StackNavigation.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {TouchableOpacity, Image} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
+
+import { Home } from '../pages/Home/Home';
+import { Detalhes } from '../pages/Detalhes/Detalhes';
+import { Cart } from '../pages/Cart/Cart'
+import { AdressAndPayment } from "../pages/AdressAndPayment/AdressAndPayment";
+
+export default function StackNavigation() {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        animation:"slide_from_right"
+      }}>
+
+        <Stack.Screen name='Home' component={Home} options={{
+          headerShown: false,
+        }}/>
+
+        <Stack.Screen name='Detalhes' component={Detalhes} options= {({ navigation }) => ({
+          title: 'Produto',
+          headerStyle: {backgroundColor: 'rgb(3,147,213)',},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Cart") } >
+              <Image
+                  source={require("../assets/shopping_cart.png")} 
+                  style={{width:40, height:40}}     
+              />
+          </TouchableOpacity>
+          ),          
+        })}/>
+        
+        <Stack.Screen name='Cart' component={Cart} options={{
+          title: 'Carrinho',
+          headerStyle: {backgroundColor: 'rgb(3,147,213)'},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+        }}/>
+
+        <Stack.Screen name='AdressAndPayment' component={AdressAndPayment} options={{
+          title: 'Informações da Compra',
+          headerStyle: {backgroundColor: 'rgb(3,147,213)'},
+          headerTintColor: '#fff',
+          headerTitleAlign: 'center',
+        }}/>
+    </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
